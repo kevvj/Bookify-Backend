159eb47aa571147abb377d4afcf360e96d049b64
kevvj <kevinjvillalba774+1@gmail.com>
2025-04-27 06:34:06 -0500

    a

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const app = express()
 const PORT = 3003
 const upload = multer()
+const pdfParse = require('pdf-parse')
 
 app.use(express.json())
 
@@ -14,10 +15,21 @@ app.get('/saludo', (req, res) => {
     res.send('jaja saludos')
 })
 
-app.post('/prueba',upload.none(), (req, res) => {
-     
-    res.send(req.body)
-    console.log(req.body)
+app.post('/prueba', upload.single('file'), async (req, res) => {
+
+    try {
+        if (!req.file) {
+          return res.status(400).json({ error: 'No se envió ningún archivo' })
+        }
+    
+        const fileBuffer = req.file.buffer
+        const data = await pdfParse(fileBuffer)
+    
+        res.json({ texto: data.text })
+      } catch (error) {
+        console.error('Error al procesar PDF:', error)
+        res.status(500).json({ error: 'Error al procesar el PDF' })
+      }
 })
 
 app.listen(PORT, () => {
